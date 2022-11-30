d04b05b377456814a112e0d73862bd9cce8923e4
Jessica Nash <jessicanashacc@gmail.com>
2022-11-30 20:00:30 +0900

    js to shrink icons

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,12 @@
 // This is where it all goes :)
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 110) {
+    document.querySelector(".button-social-icon").style.fontSize = "25px";
+  } else {
+    document.querySelector(".button-social-icon").style.fontSize = "50px";
+  }
+}
