055adcc61a46e48e954696956dd5ce1feea22edc
Jessica Nash <jessicanashacc@gmail.com>
2022-12-02 20:32:02 +0900

    right icons movement on scroll - still buggy

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,15 +6,26 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   let icons = document.querySelectorAll('.button-social-icon');
-  // let iconContainer = document.getElementById('right-buttons')
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     icons.forEach(icon => {
       icon.style.fontSize = "30px";
-    })
-    // iconContainer.style.right = "300px"
+    });
+
+    // var rightButtons = document.getElementById('right-buttons');
+    // rightButtons.style.position = "fixed";
+    // rightButtons.style.top = '90px';
+    // rightButtons.style.right = '20px';
+    // rightButtons.style.transition = "400ms"
+
+    document.getElementById('right-buttons').setAttribute('id', 'sticky-right-buttons')
   } else {
     icons.forEach(icon => {
       icon.style.fontSize = "50px";
-    })
+    });
+    // rightButtons.style.position = "absolute";
+    // rightButtons.style.top = '175px';
+    // rightButtons.style.right = '210px';
+    // rightButtons.style.transition = "400ms"
+    document.getElementById('sticky-right-buttons').setAttribute('id', 'right-buttons')
   }
 }
